4f08f55d36dde6d2d638aa41513b96eb148b72b6
Max Tong <maxtong198776@gmail.com>
2023-03-07 21:58:55 +0800

    Print out device ID.

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+
+#include <Arduino.h>
+
+#include <pas-co2-ino.hpp>
+
+/*
+ * The sensor supports 100KHz and 400KHz.
+ * You hardware setup and pull-ups value will
+ * also influence the i2c operation. You can
+ * change this value to 100000 in case of
+ * communication issues.
+ */
+#define I2C_FREQ_HZ 400000
+
+/**
+ * Create CO2 object. Unless otherwise specified,
+ * using the Wire interface
+ */
+PASCO2Ino cotwo;
+
+uint8_t prodId, revId;
+Error_t err;
+
+void setup() {
+  Serial.begin(9600);
+  delay(500);
+  Serial.println("serial initialized");
+
+  /* Initialize the i2c interface used by the sensor */
+  Wire.begin();
+  Wire.setClock(I2C_FREQ_HZ);
+
+  /* Initialize the sensor */
+  err = cotwo.begin();
+  if (XENSIV_PASCO2_OK != err) {
+    Serial.print("initialization error: ");
+    Serial.println(err);
+  }
+
+  err = cotwo.getDeviceID(prodId, revId);
+  if (XENSIV_PASCO2_OK != err) {
+    Serial.print("error: ");
+    Serial.println(err);
+  }
+
+  Serial.print("product id  : ");
+  Serial.println(prodId);
+  Serial.print("revision id : ");
+  Serial.println(revId);
+}
+
+void loop() {}
